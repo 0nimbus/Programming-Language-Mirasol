e8a2feeb9ba664ec32d377fd696b21207d902728
0nimbus <emirasol632@gmail.com>
2025-02-15 04:36:32 +0000

    Python Syntax

---
+++ 02LabExerciseMirasol.py
@@ -0,0 +1,17 @@
+#Initial invocation
+print("Hello, World!") 
+
+#Change to first name
+print("Eugene")
+
+#Without double quotes
+#print(Eugene)
+#Name error
+
+#Without parenthesis
+#print"Eugene"
+#SyntaxError
+
+#Without closing parenthesis
+#print("Eugene"
+#SyntaxError
