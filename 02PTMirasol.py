9eaaa4ad151f5e21f509062bd97663b936270496
0nimbus <emirasol632@gmail.com>
2025-02-15 23:32:55 +0000

    Python Semantics

---
+++ 02PTMirasol.py
@@ -0,0 +1,26 @@
+# Print two (2) lines of a song
+# print("I'm the last thing you'd remember")
+# print("It's been a long, lonely December")
+
+# Additional function invocation
+# print("I'm the last thing you'd remember")
+# print()
+# print("It's been a long, lonely December")
+
+# Insert \n between arguments
+# print("I'm the last thing you'd remember\nIt's been a long, lonely December")
+
+# Separate arguments with commas
+# print ("I'm the", "last thing", "you'd remember")
+
+# Add end=" " at the first function
+# print("I'm the", "last thing", "you'd remember", end=" ")
+# print("\nIt's been a long, lonely December")
+
+# Add sep=" " at the end of the second argument
+# print("I'm the", "last thing", "you'd remember", end=" ")
+# print("\nIt's been a", "long,", "lonely December", sep="---")
+
+# Do add sep and end to both functions
+# print("I'm the", "last thing", "you'd remember", sep=" ", end=" ")
+# print("\nIt's been a", "long,", "lonely December", sep="---")
